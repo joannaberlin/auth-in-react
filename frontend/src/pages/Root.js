c61b3f95f6260d19e713f846ff4f64bfe1074d40
joannaberlin <joanna.kot.ok@gmail.com>
2023-03-27 12:45:47 +0200

    Finish authentication

--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -1,19 +1,40 @@
-import { Outlet, useNavigation } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Outlet, useLoaderData, useSubmit } from 'react-router-dom';
 
 import MainNavigation from '../components/MainNavigation';
+import { getTokenDuration } from '../util/auth';
 
 function RootLayout() {
-  // const navigation = useNavigation();
+	const token = useLoaderData();
+	const submit = useSubmit();
+	// const navigation = useNavigation();
+	useEffect(() => {
+		if (!token) {
+			return;
+		}
 
-  return (
-    <>
-      <MainNavigation />
-      <main>
-        {/* {navigation.state === 'loading' && <p>Loading...</p>} */}
-        <Outlet />
-      </main>
-    </>
-  );
+		if (token === 'EXPIRED') {
+			submit(null, { action: './logout', method: 'post' });
+			return;
+		}
+
+		const tokenDuration = getTokenDuration();
+		console.log(tokenDuration);
+
+		setTimeout(() => {
+			submit(null, { action: './logout', method: 'post' });
+		}, tokenDuration);
+	}, [token, submit]);
+
+	return (
+		<>
+			<MainNavigation />
+			<main>
+				{/* {navigation.state === 'loading' && <p>Loading...</p>} */}
+				<Outlet />
+			</main>
+		</>
+	);
 }
 
 export default RootLayout;
